4dbd41367b45a391b68321e12dc0605c33036f47
Douglas Li <dougli@fb.com>
2018-01-20 11:33:58 +0000

    Stop arbitrage for a while

--- Arbitrage.js
+++ Arbitrage.js
@@ -4,6 +4,7 @@ const MIN_PROFIT_PER_ENERGY = 0.15;
 const MAX_TRADE = 10000;
 const MAX_ENERGY_PER_TICK = 10;
 const TICKS_TO_CLOSE_TRADE = 20;
+const WAIT_TIME = 19;
 
 const ARBITRAGE_RESOURCES = [
   RESOURCE_HYDROGEN,
@@ -16,12 +17,16 @@ const ARBITRAGE_RESOURCES = [
 
 const Arbitrage = {
   run: function(room) {
+    return;
     if (!room.terminal || !room.terminal.my || room.terminal.cooldown > 0) {
       return;
     }
 
     // We're in the middle of a deal here!
     let trade = room.memory.arbitrage;
+    if (trade && trade.waitTill > Game.time) {
+      return;
+    }
     if (trade) {
       delete room.memory.arbitrage;
 
@@ -38,6 +43,8 @@ const Arbitrage = {
     if (trade) {
       Game.market.deal(trade.buy.id, trade.amount, room.name);
       room.memory.arbitrage = trade;
+    } else {
+      room.memory.arbitrage = {waitTill: Game.time + WAIT_TIME};
     }
   },
 
@@ -45,44 +52,68 @@ const Arbitrage = {
     const allOrders = Game.market.getAllOrders();
 
     let bestTrade = null;
-    let bestProfit = 0;
-    for (const resourceType of ARBITRAGE_RESOURCES) {
+    for (const resource of ARBITRAGE_RESOURCES) {
       const orders = allOrders.filter(order => order.resourceType === resource);
 
       const buys = orders.filter(order => order.type === ORDER_BUY);
       const sells = orders.filter(order => order.type === ORDER_SELL);
+      if (!buys.length || !sells.length) {
+        continue;
+      }
 
-      for (const sell of sells) {
+      let best = {
+        buy: sells[0],
+        sell: buys[0],
+        profit: Number.NEGATIVE_INFINTIY,
+      };
+      let updated = false;
+      do {
+        updated = false;
         for (const buy of buys) {
-          let spread = buy.price - sell.price;
-          if (spread <= 0) {
-            continue;
-          }
-
-          let energyCost = (
-            Game.market.calcTransactionCost(1000, room.name, sell.roomName) +
-              Game.market.calcTransactionCost(1000, room.name, buy.roomName)
-          ) / 1000;
-
-          let amount = Math.min(
-            MAX_TRADE,
-            sell.amount,
-            buy.amount,
-            Math.floor(Game.market.credits / sell.price),
-            Math.floor(MAX_ENERGY_PER_TICK * TICKS_TO_CLOSE_TRADE / energyCost)
-          );
-
-          let profitPerEnergy = spread / energyCost;
-          let profit = amount * spread;
-          if (profitPerEnergy > MIN_PROFIT_PER_ENERGY && profit > bestProfit) {
-            bestTrade = {buy: sell, sell: buy, amount, profitPerEnergy, profit};
-            bestProfit = profit;
+          const result = Arbitrage.evaluateDeal(best.buy, buy, room.name);
+          if (result.profitPerEnergy > MIN_PROFIT_PER_ENERGY &&
+              result.profit > best.profit) {
+            best = result;
+            updated = true;
           }
         }
+
+        for (const sell of sells) {
+          const result = Arbitrage.evaluateDeal(sell, best.sell, room.name);
+          if (result.profitPerEnergy > MIN_PROFIT_PER_ENERGY &&
+              result.profit > best.profit) {
+            best = result;
+            updated = true;
+          }
+        }
+      } while (updated);
+      if (!bestTrade || best.profit > bestTrade.profit) {
+        bestTrade = best;
       }
     }
 
-    return bestTrade;
+    return bestTrade && bestTrade.profit > 0 ? bestTrade : null;
+  },
+
+  evaluateDeal: function(sell, buy, roomName) {
+    let spread = buy.price - sell.price;
+
+    let energyCost = (
+      Game.market.calcTransactionCost(1000, roomName, sell.roomName) +
+        Game.market.calcTransactionCost(1000, roomName, buy.roomName)
+    ) / 1000;
+
+    let amount = Math.min(
+      MAX_TRADE,
+      sell.amount,
+      buy.amount,
+      Math.floor(Game.market.credits / sell.price),
+      Math.floor(MAX_ENERGY_PER_TICK * TICKS_TO_CLOSE_TRADE / energyCost)
+    );
+
+    let profitPerEnergy = spread / energyCost;
+    let profit = amount * spread;
+    return {buy: sell, sell: buy, amount, profitPerEnergy, profit};
   }
 };
 
